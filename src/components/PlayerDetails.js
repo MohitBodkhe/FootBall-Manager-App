15bc3b55f949fe417de37e21ff4362152c7fe0e0
Mohit Bodkhe <mohitBodkhe2479@gmail.com>
2023-11-30 09:22:04 +0530

    modify rosterimporter functionality

--- src/components/PlayerDetails.js
+++ src/components/PlayerDetails.js
@@ -1,10 +1,33 @@
 import React from 'react';
 
-const PlayerDetails = ({ player }) => {
-  // Your code for rendering player details
+const PlayerDetails = ({ players }) => {
+  if (!players || players.length === 0) {
+    return null;
+  }
+
+  const headers = Object.keys(players[0]);
+
   return (
     <div>
-     
+      <h3>Player Details:</h3>
+      <table border="1">
+        <thead>
+          <tr>
+            {headers.map((header, index) => (
+              <th key={index}>{header}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {players.map((player, rowIndex) => (
+            <tr key={rowIndex}>
+              {headers.map((header, colIndex) => (
+                <td key={colIndex}>{player[header]}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
--- src/components/RosterImporter.js
+++ src/components/RosterImporter.js
@@ -1,142 +1,183 @@
-import React, { useState } from 'react';
+import React, { useState , useRef} from 'react';
+import PlayerDetails from './PlayerDetails';
 
 const RosterImporter = ({ onRosterUpdate }) => {
   const [file, setFile] = useState(null);
   const [importedData, setImportedData] = useState(null);
   const [errorFields, setErrorFields] = useState([]);
   const [showSummary, setShowSummary] = useState(false);
+  const [showPlayerDetails, setShowPlayerDetails] = useState(false);
+  const [imported, setImported] = useState(false);
+  const fileInputRef = useRef(null);
 
-  const handleFileChange = (e) => {
-    const selectedFile = e.target.files[0];
+const handleFileChange = async (e) => {
+  const selectedFile = e.target.files[0];
 
-    if (selectedFile) {
-      setFile(selectedFile);
+  if (!selectedFile) {
+    setFile(null);
+    setImportedData(null);
+    setErrorFields([]);
+    setShowSummary(false);
+    setImported(false); 
+    return;
+  }
 
-      const reader = new FileReader();
+  try {
+    const content = await selectedFile.text();
+    const { data, errors } = processCSVData(content, selectedFile);
 
-      reader.onload = (e) => {
-        const csvData = e.target.result;
-        const { data, errors } = processCSVData(csvData, selectedFile);
-
-        if (errors.length === 0) {
-          setImportedData(data);
-          setShowSummary(true);
-        } else {
-          setImportedData(null);
-          setErrorFields(errors);
-          setShowSummary(false);
-        }
-      };
-
-      reader.readAsText(selectedFile);
+    if (errors.length === 0) {
+      setImported(false); 
+      setFile(selectedFile)
+      setImportedData(data);
+      setShowSummary(true);
     } else {
-      // Reset state if no file is selected
-      setFile(null);
       setImportedData(null);
-      setErrorFields([]);
+      setErrorFields(errors);
       setShowSummary(false);
+      setImported(false);
     }
-  };
+  } catch (error) {
+    console.error('Error reading file:', error);
+    setImportedData(null);
+    setErrorFields(['Error reading the file.']);
+    setShowSummary(false);
+    setImported(false);
+  }
+};
 
-  const handleImport = () => {
-    if (file) {
-      const { data, errors } = processCSVData(file);
+const processCSVData = (csvData, file) => {
+  const rows = csvData.split('\n');
+  const headers = rows[0].split(',');
 
-      if (errors.length === 0) {
-        setImportedData(data);
-        setShowSummary(true);
-      } else {
-        setImportedData(null);
-        setErrorFields(errors);
-        setShowSummary(false);
-      }
-    }
-  };
+  const data = rows.slice(1).map((row) => {
+    const values = row.split(',');
+    return headers.reduce((obj, header, index) => {
+      obj[header.trim()] = values[index].trim();
+      return obj;
+    }, {});
+  });
 
-const processCSVData = (file) => {
-  const fileExtension = file.name.split('.').pop().toLowerCase();
   const errors = [];
 
+  // Check for empty values
+  data.forEach((item, index) => {
+    Object.keys(item).forEach((key) => {
+      if (item[key].trim() === '') {
+        errors.push(`Your sheet is missing data. Please ensure all cells are filled out.`);
+      }
+    });
+  });
+
+  // Check for file extension
+  const fileExtension = file.name.split('.').pop().toLowerCase();
   if (fileExtension !== 'csv') {
     errors.push('Invalid file format. Please select a .csv file.');
   }
 
-  const reader = new FileReader();
+  // Check for missing fields
+  const requiredFields = ['Player Name', 'Position']; // Add other required fields
+  const missingFields = requiredFields.filter((field) => !headers.includes(field));
+  if (missingFields.length > 0) {
+    errors.push(`Missing fields: ${missingFields.join(', ')}`);
+  }
 
-  reader.onload = (e) => {
-    const csvData = e.target.result;
-    const rows = csvData.split('\n');
-    const headers = rows[0].split(',');
+  return { data, errors };
+};
+const handleImport = async () => {
+  if (imported) {
+    // Reset import state and clear data
+    setImported(false);
+    setImportedData(null);
+    setShowSummary(false);
+    setShowPlayerDetails(false);
+    setErrorFields([]); // Clear any previous errors
+  } else if (file) {
+    const content = await file.text();
+    const { data, errors } = processCSVData(content, file);
 
-    const data = rows.slice(1).map((row) => {
-      const values = row.split(',');
-      return headers.reduce((obj, header, index) => {
-        obj[header.trim()] = values[index].trim();
-        return obj;
-      }, {});
-    });
-
-    // Check for empty values
-    data.forEach((item, index) => {
-      Object.keys(item).forEach((key) => {
-        if (item[key].trim() === '') {
-          errors.push(`Empty value found in row ${index + 2}, column ${key}`);
-        }
-      });
-    });
-
-    // Check for missing fields
-    const requiredFields = ['Player Name', 'Position']; // Add other required fields
-    const missingFields = requiredFields.filter((field) => !headers.includes(field));
-    if (missingFields.length > 0) {
-      errors.push(`Missing fields: ${missingFields.join(', ')}`);
+    if (errors.length === 0) {
+      setImported(true);
+      setImportedData(data);
+      setShowSummary(true);
+      setShowPlayerDetails(true);
+      // Reset the file input
+      fileInputRef.current.value = '';
+    } else {
+      setImported(false);
+      setImportedData(null);
+      setErrorFields(errors);
+      setShowSummary(false);
+      setShowPlayerDetails(false);
     }
-
-    setImportedData(data);
-    setShowSummary(true);
-  };
-
-  reader.readAsText(file);
-
-  return { errors };
+  }
 };
 
-
-  const calculatePositionCounts = () => {
-    const positionCounts = {};
-
-    if (importedData) {
-      importedData.forEach((player) => {
-        const position = player['Position'];
-        positionCounts[position] = (positionCounts[position] || 0) + 1;
-      });
-    }
-
-    return positionCounts;
+const calculatePositionCounts = () => {
+  const positionCounts = {
+    Total: 0,
+    Goalkeeper: 0,
+    Defender: 0,
+    Midfielder: 0,
+    Forward: 0,
   };
+
+  if (importedData) {
+    importedData.forEach((player) => {
+      console.log(player)
+      // Use 'Height' instead of 'Position' to get the player position
+      const position = player['Position'];
+      const trimmedPosition = position.trim();
+
+      // Log the raw position values to the console for analysis
+      console.log('Raw Position:', position);
+
+      // Check if the position is one of the standard positions
+      if (trimmedPosition === 'Goalkeeper') {
+        positionCounts.Goalkeeper++;
+      } else if (trimmedPosition === 'Defender') {
+        positionCounts.Defender++;
+      } else if (trimmedPosition === 'Midfielder') {
+        positionCounts.Midfielder++;
+      } else if (trimmedPosition === 'Forward') {
+        positionCounts.Forward++;
+      } else {
+        console.log('Unknown Position:', trimmedPosition);
+      }
+
+      // Increment the total player count
+      positionCounts.Total++;
+    });
+  }
+
+  // Log the position counts to the console
+  console.log('Position Counts:', positionCounts);
+
+  return positionCounts;
+};
 
   return (
     <div>
-      <input
+       <input
         type="file"
         accept=".csv"
         onChange={handleFileChange}
+        ref={fileInputRef}
         style={{ borderColor: errorFields.length > 0 ? 'red' : 'initial' }}
       />
 
-      {showSummary && (
-        <div>
-          <h3>Import Summary:</h3>
-          <p>Total Players: {importedData ? importedData.length : 0}</p>
-          <p>Total Goalkeepers: {calculatePositionCounts()['Goalkeeper'] || 0}</p>
-          <p>Total Defenders: {calculatePositionCounts()['Defender'] || 0}</p>
-          <p>Total Midfielders: {calculatePositionCounts()['Midfielder'] || 0}</p>
-          <p>Total Forwards: {calculatePositionCounts()['Forward'] || 0}</p>
-        </div>
-      )}
-
-      <button onClick={() => onRosterUpdate(importedData)} disabled={errorFields.length > 0}>
-        Import
+     {showSummary && (
+  <div>
+    <h3>Import Summary:</h3>
+    <p>Total Players: {importedData ? importedData.length+1 : 0}</p>
+    <p>Total Goalkeepers: {calculatePositionCounts()['Goalkeeper'] || 0}</p>
+    <p>Total Defenders: {calculatePositionCounts()['Defender'] || 0}</p>
+    <p>Total Midfielders: {calculatePositionCounts()['Midfielder'] || 0}</p>
+    <p>Total Forwards: {calculatePositionCounts()['Forward'] || 0}</p>
+  </div>
+)}
+      <button onClick={handleImport} disabled={errorFields.length > 0 || !file}>
+        {imported ? 'Re-import' : 'Import'}
       </button>
 
       {errorFields.length > 0 && (
@@ -146,6 +187,8 @@ const processCSVData = (file) => {
           ))}
         </div>
       )}
+      {importedData  && showPlayerDetails && <PlayerDetails players={importedData} />}
+
     </div>
   );
 };
